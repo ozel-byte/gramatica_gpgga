888b858642d41cbdc39c002d0e3e5db77ceffa66
Ozel <ozelcode@gmail.com>
2022-02-26 12:25:47 -0600

    diseno

--- lib/src/Pages/home_page.dart
+++ lib/src/Pages/home_page.dart
@@ -9,6 +9,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Holaaa"),
       ),
+      body: Column(
+        children: [
+          Container(
+            child: const TextField(
+              decoration: InputDecoration(hintText: 'Ingrese el codigo gppga'),
+            ),
+          ),
+          Row(
+            children: [
+              Column(
+                children: [Text("Hora"), Text("0")],
+              ),
+              Column(
+                children: [Text("Latitud"), Text(" ")],
+              ),
+              Column(
+                children: [Text("Longitud"), Text("")],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
