6a4a055d3d915955ccfabc7cc69d3361598361f2
Ozel <ozelcode@gmail.com>
2022-02-26 12:49:11 -0600

    se agrego la clase gramatica

--- lib/src/Pages/home_page.dart
+++ lib/src/Pages/home_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[300],
+        title: const Text(" Gramatica GPGGA "),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
