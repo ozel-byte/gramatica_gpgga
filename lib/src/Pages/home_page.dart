3f59d7520cd5569b41fca7846abd40f8e65ee80c
Ozel <ozelcode@gmail.com>
2022-02-27 10:58:36 -0600

    se agrego listview para el desbordamiento de la pantalla

--- lib/src/Pages/home_page.dart
+++ lib/src/Pages/home_page.dart
@@ -17,51 +17,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green[300],
-        title: const Text(" Gramatica GPGGA "),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: TextField(
-              controller: _textEditingController,
-              onChanged: (value) => _valueText = value,
-              decoration:
-                  const InputDecoration(hintText: 'Ingrese el codigo gppga'),
+        appBar: AppBar(
+          backgroundColor: Colors.green[300],
+          title: const Text(" Gramatica GPGGA "),
+        ),
+        body: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextField(
+                    controller: _textEditingController,
+                    onChanged: (value) => _valueText = value,
+                    decoration: const InputDecoration(
+                        hintText: 'Ingrese el codigo gppga'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                MaterialButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    child: const Text("Verificar"),
+                    onPressed: () {
+                      instanceGramtica.reglaUno(_valueText);
+                    }),
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [Text("Hora"), Text("0")],
+                    ),
+                    Column(
+                      children: [Text("Latitud"), Text("0")],
+                    ),
+                    Column(
+                      children: [Text("Longitud"), Text("0")],
+                    )
+                  ],
+                )
+              ],
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          MaterialButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: const Text("Verificar"),
-              onPressed: () {
-                instanceGramtica.reglaUno(_valueText);
-              }),
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [Text("Hora"), Text("0")],
-              ),
-              Column(
-                children: [Text("Latitud"), Text("0")],
-              ),
-              Column(
-                children: [Text("Longitud"), Text("0")],
-              )
-            ],
-          )
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
