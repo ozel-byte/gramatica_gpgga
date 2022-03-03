b80305e7e42dc4fd6c8c3e6e397e3fbc1b5b61c7
Kevin O <Kevinnuricumbo@gmail.com>
2022-03-03 12:32:22 -0600

    actualizado

--- lib/src/service/gramatica_gpgga.dart
+++ lib/src/service/gramatica_gpgga.dart
@@ -14,8 +14,11 @@ class GramaticaGpgga {
       'error': 's/n'
     };
 
+    final expreL = RegExp(r'^[A-z]+$');
+
     if (codigoSplit.length == 6) {
-      if (codigoSplit[0] == '\$GPGGA') {
+      if (codigoSplit[0] == '\$GPGGA' &&
+          expreL.hasMatch(codigoSplit[0].substring(1, codigoSplit[0].length))) {
         print('Correcto');
         if (_reglaHR(codigoSplit[1])) {
           if (_reglaLTyLG(codigoSplit[2], 2)) {
@@ -29,33 +32,65 @@ class GramaticaGpgga {
                       oriLa: codigoSplit[3],
                       longitud: codigoSplit[4],
                       oriLo: codigoSplit[5]);
-                  datosMapa = {'status': true, 'conversion': conversion, 'error': 'Código correcto'};
+                  datosMapa = {
+                    'status': true,
+                    'conversion': conversion,
+                    'error': 'Código correcto'
+                  };
                 } else {
                   print("Dirección de longitud incorrecta");
-                  datosMapa = {'status': false, 'conversion': 's/n', 'error': 'Direc. Long. incorrecta' };
+                  datosMapa = {
+                    'status': false,
+                    'conversion': 's/n',
+                    'error': 'Direc. Long. incorrecta'
+                  };
                 }
               } else {
                 print('La longitud es incorrecta');
-                datosMapa = {'status': false, 'conversion': 's/n', 'error': 'Long. incorrecta' };
+                datosMapa = {
+                  'status': false,
+                  'conversion': 's/n',
+                  'error': 'Long. incorrecta'
+                };
               }
             } else {
               print("Dirección de latitud incorrecta");
-              datosMapa = {'status': false, 'conversion': 's/n', 'error': 'Direc. Lati. incorrecta' };
+              datosMapa = {
+                'status': false,
+                'conversion': 's/n',
+                'error': 'Direc. Lati. incorrecta'
+              };
             }
           } else {
             print('La latitud es incorrecta');
-            datosMapa = {'status': false, 'conversion': 's/n', 'error': 'Lati. incorrecta' };
+            datosMapa = {
+              'status': false,
+              'conversion': 's/n',
+              'error': 'Lati. incorrecta'
+            };
           }
         } else {
           print("la hora es incorrecta");
-          datosMapa = {'status': false, 'conversion': 's/n', 'error': 'Hora incorrecta' };
+          datosMapa = {
+            'status': false,
+            'conversion': 's/n',
+            'error': 'Hora incorrecta'
+          };
         }
       } else {
-        datosMapa = {'status': false, 'conversion': 's/n', 'error': 'no es \$GPGGA' };
+        datosMapa = {
+          'status': false,
+          'conversion': 's/n',
+          'error': 'no es \$GPGGA'
+        };
       }
     } else {
       print("El codigo esta incorrecto");
-      datosMapa = {'status': false, 'conversion': 's/n', 'error': 'Código incorrecto' };
+      datosMapa = {
+        'status': false,
+        'conversion': 's/n',
+        'error': 'Código incorrecto'
+      };
     }
 
     print(datosMapa);
@@ -65,6 +100,9 @@ class GramaticaGpgga {
 
   bool _reglaHR(HR) {
     String H, M, S, ML;
+    final numero = RegExp(r'[0-9]+$');
+    final numero2 = RegExp(r'[0-3]+$');
+
     if (HR.length == 9) {
       H = HR.substring(0, 2);
       M = HR.substring(2, 4);
@@ -72,12 +110,14 @@ class GramaticaGpgga {
       ML = HR.substring(6, 9);
       bool auxH = false;
 
-      if (((H[0] == "0" || H[0] == "1") && "0123456789".contains(H[1])) ||
-          (H[0] == "2" && "0123".contains(H[1]))) {
-        auxH = true;
-        print('Hora correcta');
-      } else {
-        print('Hora incorrecta');
+      if (numero.hasMatch(H)) {
+        if (((H[0] == "0" || H[0] == "1") && numero.hasMatch(H[1])) ||
+            (H[0] == "2" && numero2.hasMatch(H[1]))) {
+          auxH = true;
+          print('Hora correcta');
+        } else {
+          print('Hora incorrecta');
+        }
       }
 
       if (auxH && _minOseg(M) && _minOseg(S) && ML == ".00") {
@@ -92,6 +132,9 @@ class GramaticaGpgga {
 
   bool _reglaLTyLG(LTG, tipo) {
     String G, M, ML2;
+    final numero = RegExp(r'[0-9]+$');
+    final numero2 = RegExp(r'[0-8]+$');
+    final numero3 = RegExp(r'[0-7]+$');
 
     if ((tipo == 2 && LTG.length >= 5) || (tipo == 3 && LTG.length >= 6)) {
       bool auxG = false, auxML = true, auxM = true;
@@ -99,20 +142,19 @@ class GramaticaGpgga {
       M = LTG.substring(tipo, tipo + 2);
       ML2 = LTG.substring(tipo + 3, LTG.length);
 
-      if (tipo == 2) {
-        if (("012345678".contains(G[0]) && "0123456789".contains(G[1])) ||
+      print(G);
+
+      if (tipo == 2 && numero.hasMatch(G)) {
+        if ((numero2.hasMatch(G[0]) && numero2.hasMatch(G[1])) ||
             (G[0] == "9" && G[1] == "0")) {
           auxG = true;
+          print('Latitud Correcta: $G');
         }
-        print('Latitud: $G');
+
         print(LTG[2 + tipo]);
-      } else {
-        if ((G[0] == "0" &&
-                "0123456789".contains(G[1]) &&
-                "0123456789".contains(G[2])) ||
-            (G[0] == "1" &&
-                "01234567".contains(G[1]) &&
-                "0123456789".contains(G[2])) ||
+      } else if (numero.hasMatch(G)) {
+        if ((G[0] == "0" && numero.hasMatch(G[1]) && numero.hasMatch(G[2])) ||
+            (G[0] == "1" && numero3.hasMatch(G[1]) && numero.hasMatch(G[2])) ||
             (G[0] == "1" && G[1] == "8" && G[2] == "0")) {
           auxG = true;
         }
@@ -126,13 +168,8 @@ class GramaticaGpgga {
         print('Grados incorrectos');
       }
 
-      ///aqui llamar
-
-      for (int i = 0; i < ML2.length; i++) {
-        if (!"0123456789".contains(ML2[i])) {
-          auxML = false;
-          break;
-        }
+      if (!numero.hasMatch(ML2)) {
+        auxML = false;
       }
 
       auxM = _minOseg(M);
@@ -140,15 +177,18 @@ class GramaticaGpgga {
       if (auxML && auxM) {
         if (tipo == 2 && double.parse(LTG) > 9000.0) {
           auxG = false;
+          print('ConversionBien');
         } else if (tipo == 3 && double.parse(LTG) > 18000.0) {
           auxG = false;
+          print('ConversionMal');
         }
       }
 
       if (auxG && auxM && LTG[2 + tipo] == "." && auxML) {
+        print('Longitudddddddd: ${LTG[2 + tipo]}');
         return true;
       } else {
-        print('Longitud: ${LTG[2 + tipo]}');
+        print('LongitudErr: ${LTG[2 + tipo]}');
         return false;
       }
     } else {
@@ -158,11 +198,16 @@ class GramaticaGpgga {
 
   bool _minOseg(valor) {
     bool aux = false;
-    if ("012345".contains(valor[0]) && "0123456789".contains(valor[01])) {
-      print('Minuto/segundo correcto');
-      aux = true;
-    } else {
-      print('Minuto/segundo incorrecto');
+    final numero = RegExp(r'[0-9]+$');
+    final numero2 = RegExp(r'[0-5]+$');
+
+    if (numero.hasMatch(valor)) {
+      if (numero2.hasMatch(valor[0]) && numero.hasMatch(valor[1])) {
+        print('Minuto/segundo correcto');
+        aux = true;
+      } else {
+        print('Minuto/segundo incorrecto');
+      }
     }
 
     return aux;
